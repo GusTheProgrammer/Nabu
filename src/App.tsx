d921e28dd553e73968e512398f7d1da8f7c68f42
Gus Shaal <ghiathchaal.beg@gmail.com>
2025-07-31 00:29:11 +0100

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import "./App.css";
+import Clipboard from "./Clipboard.tsx";
+
+function App() {
+  return (
+    <main className="container">
+      <h1>Welcome to Clipboardy</h1>
+        <Clipboard />
+    </main>
+  );
+}
+
+export default App;
