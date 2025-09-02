a4071b44f932ad8248a829b3a36cd4dac6f0e6cd
Jason <jason-814@hotmail.com>
2025-09-02 18:59:08 +0100

    Adding system tray and hotkey to trigger tauri app

--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -1,16 +1,78 @@
 // Learn more about Tauri commands at https://tauri.app/develop/calling-rust/
-#[tauri::command]
-fn greet(name: &str) -> String {
-    format!("Hello, {}! You've been greeted from Rust!", name)
-}
+use tauri::{
+    Manager,
+    tray::{MouseButton, MouseButtonState, TrayIconBuilder, TrayIconEvent}
+};
+
+use tauri_plugin_global_shortcut::{Code, GlobalShortcutExt, Modifiers, Shortcut, ShortcutState};
 
 #[cfg_attr(mobile, tauri::mobile_entry_point)]
 pub fn run() {
     tauri::Builder::default()
+        .setup(|app| {
+            let app_handle = app.handle(); 
+
+            TrayIconBuilder::<tauri::Wry>::new()
+                .on_tray_icon_event(move |tray, event| {
+                    match event {
+                        TrayIconEvent::Click {
+                            button: MouseButton::Left,
+                            button_state: MouseButtonState::Up,
+                            ..
+                        } => {
+                            let app = tray.app_handle();
+                            if let Some(window) = app.get_webview_window("main") {
+                                if window.is_visible().unwrap_or(false) {
+                                    let _ = window.hide();
+                                } else {
+                                    let _ = window.unminimize();
+                                    let _ = window.show();
+                                    let _ = window.set_focus();
+                                }
+                            }
+                        }
+                        _ => {},
+                    }
+                })
+                .build(app)?;
+
+            #[cfg(desktop)]
+            {
+
+                let ctrl_t_shortcut = Shortcut::new(Some(Modifiers::CONTROL), Code::KeyT);
+
+                let shortcut_app_handle = app_handle.clone();
+
+                app_handle.plugin(
+                    tauri_plugin_global_shortcut::Builder::new()
+                        .with_handler(move |_app, shortcut, event| {
+                            if shortcut == &ctrl_t_shortcut {
+                                if let ShortcutState::Pressed = event.state() {
+                                    if let Some(window) = shortcut_app_handle.get_webview_window("main") {
+                                        if window.is_visible().unwrap_or(false) {
+                                            let _ = window.hide();
+                                        } else {
+                                            let _ = window.unminimize();
+                                            let _ = window.show();
+                                            let _ = window.set_focus();
+                                        }
+                                    }
+                                }
+                            }
+                        })
+                        .build(),
+                )?;
+
+                app_handle.global_shortcut().register(ctrl_t_shortcut)?;
+            }
+
+            Ok(())
+        })
         .plugin(tauri_plugin_sql::Builder::new().build())
         .plugin(tauri_plugin_clipboard::init())
         .plugin(tauri_plugin_opener::init())
-        .invoke_handler(tauri::generate_handler![greet])
+        .invoke_handler(tauri::generate_handler![
+        ])
         .run(tauri::generate_context!())
         .expect("error while running tauri application");
 }
